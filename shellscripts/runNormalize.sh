91219ccd8d64f5fa93b006bd4d6c98fcc2c6c9b3
Smamorti <steven.mortier@hotmail.com>
2020-02-15 11:49:36 +0100

    run script for normalizing trees

---
+++ shellscripts/runNormalize.sh
@@ -0,0 +1,4 @@
+cd ../
+python normalizeTree.py newTrees/reducedTrees/tree_TTZ_LLNuNu_2018.root tree_TTZ_LLNuNu
+python normalizeTree.py newTrees/reducedTrees/tree_DY_2018.root tree_DY
+python normalizeTree.py newTrees/reducedTrees/tree_TT_2L2Nu_2018.root tree_TT_2L2Nu
