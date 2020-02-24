bba33ae97df0e3a8f64b8a42e3563bc29b3e206a
Smamorti <steven.mortier@hotmail.com>
2020-02-24 15:34:58 +0100

    plot from .pkl files

---
+++ pklPlot.py
@@ -0,0 +1,41 @@
+from utilities.pklPlotTools import makeLegend, makeCanvas, fillSubCanvas, plot
+from utilities.utils import makeYlabels, str2tuple
+#from makeHists import fillColor, fillStacked
+from ROOT import gROOT
+import pickle
+from optparse import OptionParser
+import sys
+from numpy import loadtxt
+
+# stack = "samples/newSkim_2018.stack"
+# conf = "samples/tuples_2018_newSkim.conf"
+# plt = "samples/newSkim_2018.plot"
+# year = 2018
+
+parser = OptionParser()
+parser.add_option("-f", "--inputFile", default = "histograms/histList.pkl", help = "input pkl file")
+parser.add_option("-s", "--stack", default = "samples/newSkim_2018.stack", help = "stack file")
+parser.add_option("-c", "--conf", default = "samples/tuples_2018_newSkim.conf", help = "conf file")
+parser.add_option("-p", "--plot", default = "samples/newSkim_2018.plot", help = ".plot file")
+parser.add_option("-y", "--year", default = 2018, help = "year")
+
+options, args = parser.parse_args(sys.argv[1:])
+
+channels_stack, texList, _, colorList = loadtxt(options.stack, comments = "%", unpack = True, dtype = str)
+channels_conf, files, xSecs = loadtxt(options.conf, comments = "%", unpack = True, dtype = str) 
+plotList, binList, xLabelList, xtypeList = loadtxt(options.plot, comments = "%" ,unpack = True, dtype = str, delimiter='\t')
+binList = [str2tuple(string) for string in binList]
+colorList = colorList.astype(int)
+xSecs = xSecs.astype(float)
+yLabelList = makeYlabels(xtypeList, binList)
+
+histList = pickle.load(file(options.inputFile))
+
+gROOT.SetBatch(True)
+
+leg = makeLegend(texList, histList)
+leg_2 = makeLegend(texList, histList, (0.1, 0.7, 0.3, 0.9))
+
+plot(plotList, histList, xLabelList, yLabelList, leg, leg_2, year = options.year)
+plot(plotList, histList, xLabelList, yLabelList, leg, leg_2, year = options.year, logscale = 0)
+
---
+++ utilities/pklPlotTools.py
@@ -0,0 +1,87 @@
+from ROOT import TCanvas, TLegend, THStack
+
+def makeLegend(texList, histList, position = (0.7, 0.7, 0.9, 0.9)):
+
+    leg = TLegend(position[0], position[1], position[2], position[3])
+
+    for i in range(len(texList)):
+
+        label = texList[i]
+        leg.AddEntry(histList[i][0], label, "f")
+
+    return leg
+
+def makeCanvas(horizontal, vertical):
+
+    c = TCanvas("c", "c", 550 * horizontal, 400 * vertical)
+    c.Divide(horizontal, vertical)
+
+    return c
+
+def fillSubCanvas(subCanvas, hist, xlabel, ylabel, leg, leg2, title = None, logscale = 1):
+
+    subCanvas.SetLogy(logscale)
+
+    if title:
+
+        subCanvas.SetTitle(title)
+
+    hist.Draw("HIST")
+
+    hist.GetXaxis().SetTitle(xlabel)
+    hist.GetYaxis().SetTitle(ylabel)
+
+    # hardcoded for now                                                                                                                                                                                     
+    if xlabel == "flavComp":
+
+        hist.GetXaxis().SetBinLabel(1, "ee")
+        hist.GetXaxis().SetBinLabel(2, "#mu#mu")
+        leg2.Draw()
+
+    # hardcoded for now                                                                                                                                                                                     
+    elif xlabel == "leptonMVA l1" or xlabel == "leptonMVA l2" or xlabel == "leptonMVA":
+
+        leg2.Draw()
+
+    else:
+
+        leg.Draw()
+
+    subCanvas.Update()
+
+def differentOrder(histList, i, hist, order = [2, 0, 1]):
+
+    for j in order:
+
+        hist.Add(histList[j][i])
+    
+
+def plot(plotList, histList, xLabelList, yLabelList, leg, leg2, title =  "", logscale = 1, year = "2018"):
+
+    for i in range(len(plotList)):
+        
+        if logscale:
+            scale = "log"
+        else:
+            scale = "linear"
+
+        c = makeCanvas(1, 1)
+        filename = "plots/Hist_{}_{}_{}".format(year, plotList[i], scale)
+        fillSubCanvas(c, histList[-1][i], xLabelList[i], yLabelList[i], leg, leg2, title, logscale)
+        c.SaveAs(filename + ".pdf")
+        c.SaveAs(filename + ".png")
+        c.Close()
+
+        # hardcoded (for now?)
+
+        if xLabelList[i] == "m(ll) (GeV)":
+
+            hist = THStack(xLabelList[i]," ")
+            differentOrder(histList, i, hist, order = [2, 0, 1])
+
+            c = makeCanvas(1, 1)
+            filename = "plots/Hist_{}_{}_{}_{}".format(year, plotList[i], scale, "ttunder")
+            fillSubCanvas(c, hist, xLabelList[i], yLabelList[i], leg, leg2, title, logscale)
+            c.SaveAs(filename + ".pdf")
+            c.SaveAs(filename + ".png")
+            c.Close()
