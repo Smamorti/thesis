50dec5f08131259e32362c61c5d91c075b91eccf
Smamorti <steven.mortier@hotmail.com>
2020-02-26 12:52:28 +0100

    add jet kinematics and use abs(eta) instead of eta

--- makeTreeML.py
+++ makeTreeML.py
@@ -21,7 +21,7 @@ def makeTree(inputFiles, sampleName, branches, year, xSecs):
 
     # create output file and new tree                                                                                                                                                                     
 
-    outputFile = TFile('newTrees/reducedTrees/tree_' + sampleName + '_' + year + '.root', 'RECREATE')
+    outputFile = TFile('newTrees/reducedTrees/tree_' + sampleName + '_' + str(year) + '.root', 'RECREATE')
     outputFile.cd()
     newTree = TTree('tree_' + sampleName, sampleName)
 
@@ -56,7 +56,7 @@ def fillTree(inputFile, inputTree, newTree, variables, year, xSec):
     # loop over all events, apply cuts, if all cuts apply: fill tree
 
     cutList = ["lPogLoose", "lMVA", "twoPtLeptons", "lEta", "justTwoLeptons", "twoOS", "njets", "nbjets", "twoSF", "onZ"]
-
+    year = str(year)
     if year == "2017":
         lumi = 41.53
     elif year == "2018":
@@ -113,8 +113,8 @@ def fillTree(inputFile, inputTree, newTree, variables, year, xSec):
                     variables.weight = t._weight * weight
                     variables.lPt1 = t._lPt[nLight[0]]
                     variables.lPt2 = t._lPt[nLight[1]]
-                    variables.lEta1 = t._lEta[nLight[0]]
-                    variables.lEta2 = t._lEta[nLight[1]]
+                    variables.lEta1 = np.absolute(t._lEta[nLight[0]])
+                    variables.lEta2 = np.absolute(t._lEta[nLight[1]])
                     variables.lPhi1 = t._lPhi[nLight[0]]
                     variables.lPhi2 = t._lPhi[nLight[1]]
                     variables.njets = len(nJets)
@@ -155,6 +155,29 @@ def fillTree(inputFile, inputTree, newTree, variables, year, xSec):
                     variables.mW1 = mW1
                     variables.mtop1 = mtop1
 
+                    # add all jet properties
+
+                    variables.jetPt1 = t._jetPt[nJets[0]]
+                    variables.jetEta1 = np.absolute(t._jetEta[nJets[0]])
+                    variables.jetPhi1 = t._jetPhi[nJets[0]]
+                    
+                    variables.jetPt2 = t._jetPt[nJets[1]]
+                    variables.jetEta2 = np.absolute(t._jetEta[nJets[1]])
+                    variables.jetPhi2 = t._jetPhi[nJets[1]]
+                    
+                    variables.jetPt3 = t._jetPt[nJets[2]]
+                    variables.jetEta3 = np.absolute(t._jetEta[nJets[2]])
+                    variables.jetPhi3 = t._jetPhi[nJets[2]]
+                    
+                    variables.jetPt4 = t._jetPt[nJets[3]]
+                    variables.jetEta4 = np.absolute(t._jetEta[nJets[3]])
+                    variables.jetPhi4 = t._jetPhi[nJets[3]]
+                    
+                    variables.jetPt5 = t._jetPt[nJets[4]]
+                    variables.jetEta5 = np.absolute(t._jetEta[nJets[4]])
+                    variables.jetPhi5 = t._jetPhi[nJets[4]]
+                    
+
                     newTree.Fill()
             else:
 
@@ -167,7 +190,12 @@ def fillTree(inputFile, inputTree, newTree, variables, year, xSec):
                     
 branches = ['lPt1/F', 'lPt2/F', 'lEta1/F', 'lEta2/F', 'lPhi1/F', 'lPhi2/F', 'DeltaR_1/F', 'DeltaR_b1/F', 'DeltaR_2/F', 'DeltaR_b2/F']
 branches += ['njets/I', 'nbjets/I', 'jetDeepCsv_b1/F']
-branches += ['mW1/F', 'mtop1/F']#, 'weight/F']
+branches += ['jetPt1/F', 'jetEta1/F', 'jetPhi1/F']
+branches += ['jetPt2/F', 'jetEta2/F', 'jetPhi2/F']
+branches += ['jetPt3/F', 'jetEta3/F', 'jetPhi3/F']
+branches += ['jetPt4/F', 'jetEta4/F', 'jetPhi4/F']
+branches += ['jetPt5/F', 'jetEta5/F', 'jetPhi5/F']
+branches += ['mW1/F', 'mtop1/F', 'weight/F']
 
 
 channels_conf, files, xSecs = np.loadtxt(options.conf, comments = "%", unpack = True, dtype = str)
