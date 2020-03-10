571469347f698e954af0d2fce4bcdecfe8915fdd
Smamorti <steven.mortier@hotmail.com>
2020-03-10 18:09:46 +0100

    plot tt at bottom for mll

---
+++ shellscripts/plotTTunder.sh
@@ -0,0 +1,2 @@
+cd ..
+python pklPlot.py -f histograms/histList_2018_total_tt.pkl,histograms/histList_2018_total_ttZ.pkl,histograms/histList_2018_total_ttX.pkl,histograms/histList_2018_total_ttW.pkl,histograms/histList_2018_total_other.pkl,histograms/histList_2018_total_DY.pkl -t tt,ttZ,ttX,ttW,other,DY
