291c483ab6eb3ca0c4f071c0327b6c7b7a583387
Smamorti <steven.mortier@hotmail.com>
2020-04-06 12:54:09 +0200

    some extra bash scripts

---
+++ shellscripts/plotTTunder_NN.sh
@@ -0,0 +1,2 @@
+cd ..
+python pklPlot.py -f histograms/withNN/histList_2018_total_NN_Best_FullData_18epochs_tt.pkl,histograms/withNN/histList_2018_total_NN_Best_FullData_18epochs_ttZ.pkl,histograms/withNN/histList_2018_total_NN_Best_FullData_18epochs_ttX.pkl,histograms/withNN/histList_2018_total_NN_Best_FullData_18epochs_ttW.pkl,histograms/withNN/histList_2018_total_NN_Best_FullData_18epochs_other.pkl,histograms/withNN/histList_2018_total_NN_Best_FullData_18epochs_DY.pkl -t tt,ttZ,ttX,ttW,other,DY
