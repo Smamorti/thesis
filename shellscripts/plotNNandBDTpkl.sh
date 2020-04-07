ebe3223b17cd0a2eb728dc7111c76e0946632c7d
Smamorti <steven.mortier@hotmail.com>
2020-04-07 13:39:55 +0200

    quick script to make a lot off plots

---
+++ shellscripts/plotNNandBDTpkl.sh
@@ -0,0 +1,10 @@
+cd ..
+python pklPlot.py -f histograms/BDT_final_fullData_wp\=0.757869/histList_2018_total.pkl
+python pklPlot.py -f histograms/BDT_final_fullData_wp\=0.758521/histList_2018_total.pkl
+python pklPlot.py -f histograms/BDT_final_fullData_wp\=0.759173/histList_2018_total.pkl
+python pklPlot.py -f histograms/BDT_final_fullData_wp\=0.85986/histList_2018_total.pkl
+python pklPlot.py -f histograms/BDT_final_fullData_wp\=0.960548/histList_2018_total.pkl
+
+python pklPlot.py -f histograms/NN_Best_FullData_18epochs_wp\=0.763306/histList_2018_total.pkl
+python pklPlot.py -f histograms/NN_Best_FullData_18epochs_wp\=0.813306/histList_2018_total.pkl
+python pklPlot.py -f histograms/NN_Best_FullData_18epochs_wp\=0.863306/histList_2018_total.pkl
