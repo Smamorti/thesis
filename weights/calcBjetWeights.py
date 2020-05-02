d46a77839f3273e3eff0645f3279eabfa404cb72
Smamorti <steven.mortier@hotmail.com>
2020-05-02 21:54:47 +0200

    checklist

--- weights/calcBjetWeights.py
+++ weights/calcBjetWeights.py
@@ -5,6 +5,23 @@ import sys
 from optparse import OptionParser
 import os
 
+######
+
+# 1) only have udsg in csv file, for other SF = 1?
+# 2) what if the jet eta/pt does not fall within the required ranges?
+#        remember that for every jet the pt is > 30 and abs(eta) < 2.4
+#        --> should I care about jet pt/eta? 
+# 3) basically same question, but for the discriminant value
+
+#####
+
+
+## TO DO 
+
+# Read in the csv ONCE when starting the making of histos
+# From the csv, select the formula(s) corresponding to the region we want to look for
+#    For example: loose tagging, inclusive and the up-correction leaves you w/ only 1 formula!
+
 parser = OptionParser()
 parser.add_option("-c", "--csv", default = "DeepCSV_94XSF_WP_V4_B_F.csv", help = "input csv file?")
 options, args = parser.parse_args(sys.argv[1:])
