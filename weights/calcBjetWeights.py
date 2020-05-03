cec369fb012309d3885ee2c85001296415e973d6
Smamorti <steven.mortier@hotmail.com>
2020-05-03 16:44:30 +0200

    add SF and eff list calc

--- weights/calcBjetWeights.py
+++ weights/calcBjetWeights.py
@@ -30,7 +30,15 @@ import os
 # sysType: add to arg parser
 # measurementType: to do some pre-cleaning, we will give ALL arrays to final program, just less entries
 # operatingPoint: probably not worth adding to argparser, only keep loose, hardcode
+#
+# make function "getBtagArrays" --> returns measurementType, pt low, pt up, hadronFlavor and formulas
+#
 
+
+# bTagEff_loose_udsg
+# bTagEff_loose_charm
+# bTagEff_loose_beauty
+# open these in another place, probably leptonPlots.py!
 
 
 parser = OptionParser()
@@ -40,25 +48,166 @@ options, args = parser.parse_args(sys.argv[1:])
 def getWeightFormula(formula, i):
 
     gROOT.ProcessLine('#include <string>')
-    gROOT.ProcessLine('TF1 f'+str(i)+'("f'+str(i)+'", '+formula[i].strip()+');')     
+    gROOT.ProcessLine('TF1 f'+str(i)+'("f'+str(i)+'", '+formula.strip()+');')     
 
-    return  getattr(ROOT, 'f'+str(i))
+    return getattr(ROOT, 'f'+str(i))
+
+def getBtagArrays(sysType):
+
+    csvFile = "DeepCSV_94XSF_WP_V4_B_F.csv"
+
+    OperatingPoint, measurementType, sysTypes, jetFlavor, etaMin, etaMax, ptMin, ptMax, discrMin, discrMax, formulas= np.loadtxt(options.csv, unpack = True, skiprows = 1, delimiter = ', ', dtype = str)
+
+    keepThese = (sysTypes == sysType) & (measurementType == 'comb') & (OperatingPoint== '0')
+#    keepThese = (sysTypes == sysType) & ((measurementType == 'incl') | (measurementType == 'comb'))& (OperatingPoint== '0')
+
+    forms = formulas[keepThese]
+
+    mesTypes = measurementType[keepThese]
+
+    ptLow = ptMin[keepThese]
+
+    ptHigh = ptMax[keepThese]
+
+    hadronFlavor = jetFlavor[keepThese]
+
+    tformulas = TList()
+
+    for i in range(len(forms)):
+
+        f = getWeightFormula(forms[i], i)
+
+        tformulas.Add(f)
+    
+    inclForm = (sysTypes == sysType) & (measurementType == 'incl') & (OperatingPoint== '0')
+
+    inclFormula = getWeightFormula(formulas[inclForm][0], 'incl')
+
+    return mesTypes, hadronFlavor, ptLow, ptHigh, tformulas, inclFormula
 
 
-OperatingPoint, measurementType, sysType, jetFlavor, etaMin, etaMax, ptMin, ptMax, discrMin, discrMax, formulas = np.loadtxt(options.csv, unpack = True, skiprows = 1, delimiter = ', ', dtype = str)
+def calcSF(tree, nJets, measurementTypes, jetFlavors, ptLow, ptHigh, tformulas, inclusiveFormula, JEC):
 
-print(formulas[(sysType == 'up') & ((measurementType == 'incl') | (measurementType == 'comb'))& (OperatingPoint == '0')])
+    # hardcoded that there is always just one formula for the inclusive part!
+
+    SFs = []
+
+    for i in nJets:
+
+        jetFlavor = tree._jetHadronFlavor[i]
+        
+        if JEC == 'nominal':
+
+            jetPt = tree._jetPt[i]
+
+        elif JEC == 'up':
+
+            jetPt = tree._jetPt_JECUp[i]
+
+        elif JEC == 'down':
+
+            jetPt = tree._jetPt_JECDown[i]
+
+        if jetFlavor == 0:
+
+            SF = inclusiveFormula.Eval(jetPt)
+
+        else:
+
+            if jetFlavor == 4:
+
+                valid = (jetFlavors == 1)
+
+            else:
+
+                valid == (jetFlavors == 0)
+
+            ptMax = ptHigh[valid]
+            tforms = tformulas[valid]
+        
+
+            print(ptHigh)
+            print(valid)
+            print(ptMax)
+
+            j = 0
+
+            while jetPt > ptMax[j] and j < len(ptMax) - 1:
+
+                j += 1
+
+            SF = tforms[j].Eval(jetPt)
+
+        SFs.append(SF)
+
+    return SFs
+        
+def calcEfficiencies(tree, nJets, udsgHist, cHist, bHist, JEC):
+
+    effList = []
+
+    for i in nJets:
+        
+        jetFlavor = tree._jetHadronFlavor[i]
+
+        if JEC == 'nominal':
+
+            jetPt = tree._jetPt[i]
+
+        elif JEC == 'up':
+
+            jetPt = tree._jetPt_JECUp[i]
+
+        elif JEC == 'down':
+
+            jetPt = tree._jetPt_JECDown[i]
+
+        eta = np.absolute(tree._jetEta[i])
+
+        if jetFlavor == 0:
+
+            effList.append(udsgHist.GetbinContent(jetPt, eta))
+
+        elif jetFlavor == 4:
+
+            effList.append(cHist.GetbinContent(jetPt, eta))
+
+        elif jetFlavor == 5:
+
+            effList.append(bHist.GetbinContent(jetPt, eta))
+
+    return effList
+
+def calcBtagWeight(tree, nJets, effList, SFlist):
+
+    pMC = 1
+    pData = 1
+
+    for i in nJets:
+
+        if tree._jetDeepCsv_b[i] > 0.4941:
+
+            pMC *= effList[i]
+            pData *= (SFlist[i] * effList[i])
+
+        else:
+
+            pMC *= (1 - effList[i])
+            pData *= (1 - SFlist[i]* effList[i])
+
+    return pData / pMC
 
 
-# tformulas = TList()
+measurementTypes, jetFlavors, ptLow, ptHigh, tformulas, inclFormula = getBtagArrays(sysType = 'up')
 
-# for i in range(len(formulas)):
+print(measurementTypes)
+print(jetFlavors)
+print(ptLow)
+print(ptHigh)
 
-#     f = getWeightFormula(formulas, i)
+print(inclFormula.Eval(50))
 
-#     tformulas.Add(f)
+for x in tformulas:
 
-# for x in tformulas:
-
-#     print(x.Eval(50))
+    print(x.Eval(50))
 
