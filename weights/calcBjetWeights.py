1631b264cb1f39e4f8a11f1ce087dacab479be42
Smamorti <steven.mortier@hotmail.com>
2020-05-04 11:33:01 +0200

    combine everything into one func to reduce amount of for loops

--- weights/calcBjetWeights.py
+++ weights/calcBjetWeights.py
@@ -86,65 +86,170 @@ def getBtagArrays(sysType):
     return mesTypes, hadronFlavor, ptLow, ptHigh, tformulas, inclFormula
 
 
-def calcSF(tree, nJets, measurementTypes, jetFlavors, ptLow, ptHigh, tformulas, inclusiveFormula, JEC):
+# def calcSF(tree, nJets, measurementTypes, jetFlavors, ptLow, ptHigh, tformulas, inclusiveFormula, JEC):
 
-    # hardcoded that there is always just one formula for the inclusive part!
+#     # hardcoded that there is always just one formula for the inclusive part!
 
-    SFs = []
+#     SFs = []
 
-    for i in nJets:
+#     for i in nJets:
 
-        jetFlavor = tree._jetHadronFlavor[i]
+#         jetFlavor = tree._jetHadronFlavor[i]
         
-        if JEC == 'nominal':
+#         if JEC == 'nominal':
 
-            jetPt = tree._jetPt[i]
+#             jetPt = tree._jetPt[i]
 
-        elif JEC == 'up':
+#         elif JEC == 'up':
 
-            jetPt = tree._jetPt_JECUp[i]
+#             jetPt = tree._jetPt_JECUp[i]
 
-        elif JEC == 'down':
+#         elif JEC == 'down':
 
-            jetPt = tree._jetPt_JECDown[i]
+#             jetPt = tree._jetPt_JECDown[i]
 
-        if jetFlavor == 0:
+#         if jetFlavor == 0:
 
-            SF = inclusiveFormula.Eval(jetPt)
+#             SF = inclusiveFormula.Eval(jetPt)
 
-        else:
+#         else:
 
-            if jetFlavor == 4:
+#             if jetFlavor == 4:
 
-                valid = (jetFlavors == 1)
+#                 valid = (jetFlavors == 1)
 
-            else:
+#             else:
 
-                valid == (jetFlavors == 0)
+#                 valid == (jetFlavors == 0)
 
-            ptMax = ptHigh[valid]
-            tforms = tformulas[valid]
+#             ptMax = ptHigh[valid]
+#             tforms = tformulas[valid]
         
 
-            print(ptHigh)
-            print(valid)
-            print(ptMax)
+#             print(ptHigh)
+#             print(valid)
+#             print(ptMax)
 
-            j = 0
+#             j = 0
 
-            while jetPt > ptMax[j] and j < len(ptMax) - 1:
+#             while jetPt > ptMax[j] and j < len(ptMax) - 1:
 
-                j += 1
+#                 j += 1
 
-            SF = tforms[j].Eval(jetPt)
+#             SF = tforms[j].Eval(jetPt)
 
-        SFs.append(SF)
+#         SFs.append(SF)
 
-    return SFs
+#     return SFs
         
-def calcEfficiencies(tree, nJets, udsgHist, cHist, bHist, JEC):
+# def calcEfficiencies(tree, nJets, udsgHist, cHist, bHist, JEC):
 
-    effList = []
+#     effList = []
+
+#     for i in nJets:
+        
+#         jetFlavor = tree._jetHadronFlavor[i]
+
+#         if JEC == 'nominal':
+
+#             jetPt = tree._jetPt[i]
+
+#         elif JEC == 'up':
+
+#             jetPt = tree._jetPt_JECUp[i]
+
+#         elif JEC == 'down':
+
+#             jetPt = tree._jetPt_JECDown[i]
+
+#         eta = np.absolute(tree._jetEta[i])
+
+#         if jetFlavor == 0:
+
+#             effList.append(udsgHist.GetbinContent(jetPt, eta))
+
+#         elif jetFlavor == 4:
+
+#             effList.append(cHist.GetbinContent(jetPt, eta))
+
+#         elif jetFlavor == 5:
+
+#             effList.append(bHist.GetbinContent(jetPt, eta))
+
+#     return effList
+
+
+
+
+# def calcEffandSR(tree, nJets, udsgHist, cHist, bHist, jetFlavors, ptHigh, tformulas, inclusiveFormula, JEC):
+
+#     effList = []
+#     SFs = []
+
+#     for i in nJets:
+        
+#         jetFlavor = tree._jetHadronFlavor[i]
+
+#         if JEC == 'nominal':
+
+#             jetPt = tree._jetPt[i]
+
+#         elif JEC == 'up':
+
+#             jetPt = tree._jetPt_JECUp[i]
+
+#         elif JEC == 'down':
+
+#             jetPt = tree._jetPt_JECDown[i]
+
+#         eta = np.absolute(tree._jetEta[i])
+
+#         if jetFlavor == 0:
+
+#             effList.append(udsgHist.GetbinContent(jetPt, eta))
+#             SF = inclusiveFormula.Eval(jetPt)
+
+#         else:
+
+#             if jetFlavor == 4:
+
+#                 effList.append(cHist.GetbinContent(jetPt, eta))
+#                 valid = (jetFlavors == 1)
+
+
+#             else:
+
+#                 effList.append(bHist.GetbinContent(jetPt, eta))
+#                 valid == (jetFlavors == 0)
+
+
+#             ptMax = ptHigh[valid]
+#             tforms = tformulas[valid]
+        
+#             print(ptHigh)
+#             print(valid)
+#             print(ptMax)
+
+#             j = 0
+
+#             while jetPt > ptMax[j] and j < len(ptMax) - 1:
+
+#                 j += 1
+
+#             SF = tforms[j].Eval(jetPt)
+            
+#         SFs.append(SF)
+
+
+#     return effList, SFs
+
+
+
+
+def calcBtagWeight(tree, nJets, udsgHist, cHist, bHist, jetFlavors, ptHigh, tformulas, inclusiveFormula, JEC):
+
+    pMC = 1
+    pData = 1
 
     for i in nJets:
         
@@ -166,48 +271,93 @@ def calcEfficiencies(tree, nJets, udsgHist, cHist, bHist, JEC):
 
         if jetFlavor == 0:
 
-            effList.append(udsgHist.GetbinContent(jetPt, eta))
-
-        elif jetFlavor == 4:
-
-            effList.append(cHist.GetbinContent(jetPt, eta))
-
-        elif jetFlavor == 5:
-
-            effList.append(bHist.GetbinContent(jetPt, eta))
-
-    return effList
-
-def calcBtagWeight(tree, nJets, effList, SFlist):
-
-    pMC = 1
-    pData = 1
-
-    for i in nJets:
-
-        if tree._jetDeepCsv_b[i] > 0.4941:
-
-            pMC *= effList[i]
-            pData *= (SFlist[i] * effList[i])
+            eff = udsgHist.GetbinContent(jetPt, eta)
+            SF = inclusiveFormula.Eval(jetPt)
 
         else:
 
-            pMC *= (1 - effList[i])
-            pData *= (1 - SFlist[i]* effList[i])
+            if jetFlavor == 4:
+
+                eff = cHist.GetbinContent(jetPt, eta)
+                valid = (jetFlavors == 1)
+
+
+            else:
+
+                eff = bHist.GetbinContent(jetPt, eta)
+                valid == (jetFlavors == 0)
+
+
+            ptMax = ptHigh[valid]
+            tforms = tformulas[valid]
+        
+            print(ptHigh)
+            print(valid)
+            print(ptMax)
+
+            j = 0
+
+            while jetPt > ptMax[j] and j < len(ptMax) - 1:
+
+                j += 1
+
+            SF = tforms[j].Eval(jetPt)
+            
+        pMC, pData = updateBtagWeight(tree, i, eff, SF, pMC, pData)
+
 
     return pData / pMC
 
 
-measurementTypes, jetFlavors, ptLow, ptHigh, tformulas, inclFormula = getBtagArrays(sysType = 'up')
 
-print(measurementTypes)
-print(jetFlavors)
-print(ptLow)
-print(ptHigh)
+def updateBtagWeight(tree, i, eff, SF, pMC, pData):
 
-print(inclFormula.Eval(50))
+    if tree._jetDeepCsv_b[i] > 0.4941:
 
-for x in tformulas:
+        pMC *= eff
+        pData *= (SF * eff)
 
-    print(x.Eval(50))
+    else:
+
+        pMC *= (1 - eff)
+        pData *= (1 - Sf * eff)
+
+    return pMC, pData
+
+# def calcBtagWeight(tree, nJets, effList, SFlist):
+
+#     pMC = 1
+#     pData = 1
+
+#     for i in nJets:
+
+#         if tree._jetDeepCsv_b[i] > 0.4941:
+
+#             pMC *= effList[i]
+#             pData *= (SFlist[i] * effList[i])
+
+#         else:
+
+#             pMC *= (1 - effList[i])
+#             pData *= (1 - SFlist[i]* effList[i])
+
+#     return pData / pMC
+
+
+# for testing
+
+if __name__ == "__main__":
+
+    measurementTypes, jetFlavors, ptLow, ptHigh, tformulas, inclFormula = getBtagArrays(sysType = 'up')
+
+    print(measurementTypes)
+    print(jetFlavors)
+    print(ptLow)
+    print(ptHigh)
+
+    print(inclFormula.Eval(50))
+
+    for x in tformulas:
+
+        print(x.Eval(50))
 
